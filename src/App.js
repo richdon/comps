1f69dbf4af485ac6397d9ae827e8f951f1a626b8
Richard Donald <richard_donald92@outlook.com>
2024-04-30 18:16:46 -0400

    button component with tailwind and propTypes

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
+import Button from "./Button";
+
 export default function app() {
-    return <div>App</div>
+    return (<div>
+        <div>
+            <Button success rounded outline>Click here!</Button>
+        </div>
+        <div>
+            <Button primary outline>Buy Now!</Button>
+        </div>
+        <div>
+            <Button warning>Hide Ads!</Button>
+        </div>
+        <div>
+            <Button secondary outline>Add to Cart!</Button>
+        </div>
+        <div>
+            <Button danger rounded>Cancel!</Button>
+        </div>
+    </div>)
 }
---
+++ src/Button.js
@@ -0,0 +1,30 @@
+import PropTypes from "prop-types";
+
+function Button({
+                    children,
+                    primary,
+                    secondary,
+                    success,
+                    warning,
+                    danger,
+                    outline,
+                    rounded
+                }) {
+    return <button>{children}</button>
+}
+
+Button.propTypes = {
+    checkVariationValue: ({primary, secondary, success, warning, danger}) => {
+        const count = Number(!!primary) +
+            + Number(!!secondary)
+            + Number(!!success)
+            + Number(!!warning)
+            + Number(!!danger)
+        if (count !== 1) {
+            return new Error('only one of primary, secondary, success, warning, danger can be true');
+        }
+
+    }
+}
+
+export default Button;
