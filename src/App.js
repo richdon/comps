ad8a51450c3591e61d537ecfaeb5649881e1d570
Richard Donald <richard_donald92@outlook.com>
2024-05-06 18:29:26 -0400

    wrap up dropdown

--- src/App.js
+++ src/App.js
@@ -1,18 +1,4 @@
-import Dropdown from "./components/Dropdown";
-import {useState} from "react";
 
 export default function App() {
-
-    const [selection, setSelection] = useState(null)
-
-    const handleSelect = (option) => {
-        setSelection(option)
-    }
-
-    const options = [
-        {label: 'Red', value: 'red'},
-        {label: 'Green', value: 'green'},
-        {label: 'Blue', value: 'blue'}
-    ]
-    return <Dropdown options={options} value={selection} onChange={handleSelect}/>
+    return <div></div>
 }
--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,9 +1,29 @@
-import {useState} from "react";
+import {useState, useEffect, useRef} from "react";
 import {GoChevronDown} from "react-icons/go";
 import {Panel} from "./Panel";
 
 export default function Dropdown({options, value, onChange}) {
     const [isOpen, setIsOpen] = useState(false)
+    const divEl = useRef();
+
+    useEffect(()=>{
+        const handler = (event) => {
+
+            if (!divEl.current){
+                return;
+            }
+
+            if (!divEl.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        };
+        document.addEventListener('click', handler, true)
+
+        return () => {
+            document.removeEventListener('click', handler)
+        };
+    }, [])
+
     const handleClick = () => {
         setIsOpen(!isOpen);
     }
@@ -22,7 +42,7 @@ export default function Dropdown({options, value, onChange}) {
         </div>
     });
 
-    return <div className="w-48 relative">
+    return <div ref={divEl} className="w-48 relative">
         <Panel className="flex justify-between items-center cursor-pointer"
                onClick={handleClick}>{value?.label || 'Select...'}<GoChevronDown className="text-lg"/>
         </Panel>
