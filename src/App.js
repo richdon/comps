6df027e6ed7189173ab7ce2d39187cbd5bf2824a
Richard Donald <richard_donald92@outlook.com>
2024-05-19 15:06:30 -0400

    modal component skeleton

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Route from "./components/Route";
 import AccordionPage from "./pages/AccordionPage";
 import DropdownPage from "./pages/DropdownPage";
 import ButtonPage from "./pages/ButtonPage";
+import ModalPage from "./pages/ModalPage";
 export default function App() {
     return <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
         <Sidebar/>
@@ -15,6 +16,9 @@ export default function App() {
             </Route>
             <Route path="/buttons">
                 <ButtonPage/>
+            </Route>
+            <Route path="/modal">
+                <ModalPage/>
             </Route>
 
         </div>
---
+++ src/pages/ModalPage.js
@@ -0,0 +1,7 @@
+import Modal from "../components/modal";
+
+export default function ModalPage(){
+    return <div>
+        <Modal/>
+    </div>
+}
