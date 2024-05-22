15fbda38d23ccd386901907afd4e6fe68f70b217
Richard Donald <richard_donald92@outlook.com>
2024-05-21 22:18:45 -0400

    counter after useReducer

--- src/pages/CounterPage.js
+++ src/pages/CounterPage.js
@@ -1,31 +1,100 @@
 import Button from "../components/Button";
-import {useState} from "react";
+import {useReducer} from "react";
 import {Panel} from "../components/Panel";
 
+const INCREMENT_COUNT = 'increment'
+const DECREMENT_COUNT = 'decrement'
+const CHANGE_VALUE = 'change-value'
+const ADD_VALUE_TO_COUNT = 'add-to-count'
+const reducer = (state, action) => {
+    // whatever gets returned will be new state. no requests/async await
+    // if (action.type === INCREMENT_COUNT) {
+    //     return {
+    //         ...state,
+    //         count: state.count + 1
+    //     }
+    // }
+    // if (action.type === DECREMENT_COUNT) {
+    //     return {
+    //         ...state,
+    //         count: state.count - 1
+    //     }
+    // }
+    // if (action.type === CHANGE_VALUE) {
+    //     return {
+    //         ...state,
+    //         valueToAdd: action.payload,
+    //         count: state.count + action.payload
+    //     }
+    // }
+    // return state
+
+    switch (action.type) {
+        case INCREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count + 1
+            };
+        case DECREMENT_COUNT:
+            return {
+                ...state,
+                count: state.count - 1
+            };
+        case CHANGE_VALUE:
+            return {
+                ...state,
+                valueToAdd: action.payload
+            };
+        case ADD_VALUE_TO_COUNT:
+            return {
+                ...state,
+                count: state.count + state.valueToAdd,
+                valueToAdd: 0
+            }
+        default:
+            throw new Error('unexpected action type: '+ action.type);
+
+    }
+}
 
 export default function CounterPage({initialCount}) {
-    const [count, setCount] = useState(initialCount);
-    const [valueToAdd, setValueToAdd] = useState(0)
+    // const [count, setCount] = useState(initialCount);
+    // const [valueToAdd, setValueToAdd] = useState(0);
+
+    const [state, dispatch] = useReducer(reducer, {
+        count: initialCount,
+        valueToAdd: 0,
+    })
+
     const increment = () => {
-        setCount(count + 1);
+        // setCount(count + 1);
+        dispatch({type: 'increment'});
     }
     const decrement = () => {
-        setCount(count - 1);
+        // setCount(count - 1);
+        dispatch({type: 'decrement'});
     }
 
     const handleChange = (event) => {
         const value = parseInt(event.target.value) || 0;
-        setValueToAdd(value);
+        // setValueToAdd(value);
+        dispatch({
+            type: 'change-value',
+            payload: value
+        });
     }
 
     const handleSubmit = (event) => {
         event.preventDefault();
-        setCount(count + valueToAdd)
-        setValueToAdd(0)
+        // setCount(count + valueToAdd)
+        // setValueToAdd(0)
+        dispatch({
+            type: ADD_VALUE_TO_COUNT,
+        })
     }
 
     return (<Panel className="m-3">
-        <h1 className="text-lg">Count is {count}</h1>
+        <h1 className="text-lg">Count is {state.count}</h1>
         <div className="flex flex-row">
             <Button onClick={increment}>Increment</Button>
             <Button onClick={decrement}>Decrement</Button>
@@ -33,7 +102,7 @@ export default function CounterPage({initialCount}) {
 
         <form onSubmit={handleSubmit}>
             <label>Add a lot!</label>
-            <input value={valueToAdd || ""} onChange={handleChange} type="number"
+            <input value={state.valueToAdd || ""} onChange={handleChange} type="number"
                    className="p-1 m-3 bg-gray-50 border border-gray-300"/>
             <Button>Add it!</Button>
         </form>
